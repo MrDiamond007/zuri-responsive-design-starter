12e606b05971b4fa3a37f9b115599e77e9844ec1
unknown <ganiyubabatunde17@gmail.com>
2021-04-10 05:49:35 +0100

    addeded media query

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav-links");
+
+
+hamburger.addEventListener("click", () => {
+    nav-links.classList.toggle("show");
+});
